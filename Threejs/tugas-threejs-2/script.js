6bf25189099ab3e52f37e4aaa21063af2a3bbd3e
mirzaq19 <mirzaqarjap@gmail.com>
2021-10-16 19:46:20 +0700

    update: fix game

--- Threejs/tugas-threejs-2/script.js
+++ Threejs/tugas-threejs-2/script.js
@@ -1,3 +1,7 @@
+const SELECT = document.querySelector("#seleksi");
+const SCORE = document.querySelector("#score");
+const PLAY_BUTTON = document.querySelector("#play-button");
+const GAMEOVER = document.querySelector("#gameover");
 //========== Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -10,8 +14,9 @@ scene.add(new THREE.DirectionalLight(0xffffff, 0.8));
 scene.add(new THREE.AmbientLight(0xffffbb, 0.5));
 
 //========== Create Geometry
+const OBJECT_TOTAL = 50;
 let copBuffer;
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < OBJECT_TOTAL / 2; i++) {
   copBuffer = createCouple();
   // console.log(copBuffer);
   scene.add(copBuffer[0]);
@@ -23,6 +28,53 @@ const sizes = {
   width: 0.9 * window.innerWidth,
   height: 0.9 * window.innerHeight,
 };
+
+//========= Pengaturan Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 0, 50);
+
+const orbitControls = new THREE.OrbitControls(camera, canvas);
+orbitControls.target.set(0, 5, 0);
+orbitControls.enableZoom = false;
+orbitControls.update();
+
+//============= Render
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//=========== RayCaster
+const rayCast = new THREE.Raycaster();
+// rayCast.layers.set(1);
+const mouse = new THREE.Vector2();
+mouse.x = mouse.y = -1;
+
+//=========== Interactive Action
+let gameOver = true;
+let selected = [];
+let spawnSpeed = 0.005;
+let bufferSpeed = 0;
+let visibles = [];
+let notVisibles = numberInRange(2, OBJECT_TOTAL + 1);
+
+PLAY_BUTTON.addEventListener("click", () => {
+  gameOver = false;
+  selected = [];
+  spawnSpeed = 0.005;
+  bufferSpeed = 0;
+  visibles = [];
+  notVisibles = numberInRange(2, OBJECT_TOTAL + 1);
+  PLAY_BUTTON.disabled = true;
+  SCORE.innerHTML = 0;
+  GAMEOVER.style.display = "none";
+});
 
 window.addEventListener("resize", () => {
   // Update sizes
@@ -38,111 +90,75 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-//========= Pengaturan Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(0, 0, 25);
-
-const orbitControls = new THREE.OrbitControls(camera, canvas);
-orbitControls.target.set(0, 5, 0);
-orbitControls.enableZoom = false;
-orbitControls.update();
-
-//============= Render
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  // alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-//=========== RayCaster
-const rayCast = new THREE.Raycaster();
-// rayCast.layers.set(1);
-const mouse = new THREE.Vector2();
-mouse.x = mouse.y = -1;
-
-//=========== Interactive Action
-let selected = [];
-
-// const pointControls = new THREE.PointerLockControls(
-//   camera,
-//   renderer.domElement
-// );
-// const clock = new THREE.Clock();
-// let keyboard = [];
-// canvas.addEventListener("click", () => {
-//   pointControls.lock();
-// });
-// document.addEventListener("keydown", (e) => {
-//   keyboard[e.key] = true;
-// });
-// document.addEventListener("keyup", (e) => {
-//   keyboard[e.key] = false;
-// });
 canvas.addEventListener("click", (e) => {
-  mouse.x = (e.offsetX / sizes.width) * 2 - 1;
-  mouse.y = -(e.offsetY / sizes.height) * 2 + 1;
-  rayCast.setFromCamera(mouse, camera);
-  let items = rayCast.intersectObjects(scene.children, false);
-  if (items.length) {
-    selected.push(items[0].object);
-    items[0].object.material.color.set(0xffffff);
-  }
+  if (!gameOver) {
+    mouse.x = (e.offsetX / sizes.width) * 2 - 1;
+    mouse.y = -(e.offsetY / sizes.height) * 2 + 1;
+    rayCast.setFromCamera(mouse, camera);
+    let items = rayCast.intersectObjects(scene.children, false);
+    let selectFirstItem = true;
+    items.forEach((item) => {
+      if (item.object.visible && !item.object.click && selectFirstItem) {
+        selected.push(item.object);
+        item.object.material.color.set(0xffffff);
+        item.object.click = true;
+        selectFirstItem = false;
+      }
+    });
 
-  if (selected.length == 2) {
-    console.log(selected);
-    if (selected[0].coupleColor == selected[1].coupleColor) {
-      console.log("cocok");
-      selected.forEach((select) => {
-        select.visible = false;
-      });
-    } else {
-      console.log("salah");
-      selected.forEach((select) => {
-        select.material.color.set(select.coupleColor);
-      });
+    if (selected.length == 2) {
+      if (selected[0].coupleColor == selected[1].coupleColor) {
+        console.log("cocok");
+        selected.forEach((select) => {
+          select.visible = false;
+          select.material.color.set(select.coupleColor);
+          select.click = false;
+          for (let i = 2; i < OBJECT_TOTAL + 1; i++) {
+            if (scene.children[i] === select) {
+              console.log("ketemu");
+              for (let j = 0; j < visibles.length; j++) {
+                if (visibles[j] == i) {
+                  notVisibles.push(visibles.splice(j, 1));
+                  break;
+                }
+              }
+              break;
+            }
+          }
+        });
+        SCORE.innerHTML++;
+      } else {
+        console.log("salah");
+        selected.forEach((select) => {
+          select.material.color.set(select.coupleColor);
+          select.click = false;
+        });
+      }
+      selected = [];
     }
-    selected = [];
+    SELECT.innerHTML = selected.length;
   }
-
-  // console.log(selected);
-  console.log(scene.children);
 });
-
-// const processingKeyboard = (delta) => {
-//   const SPEED = 30;
-//   const actualSpeed = SPEED * delta;
-//   if (keyboard["q"]) camera.position.y += actualSpeed;
-//   if (keyboard["e"]) camera.position.y += -actualSpeed;
-//   if (keyboard["w"]) pointControls.moveForward(actualSpeed);
-//   if (keyboard["s"]) pointControls.moveForward(-actualSpeed);
-//   if (keyboard["a"]) pointControls.moveRight(-actualSpeed);
-//   if (keyboard["d"]) pointControls.moveRight(actualSpeed);
-// };
-// const processingKeyboard = (delta) => {
-//   const SPEED = 30;
-//   const actualSpeed = SPEED * delta;
-//   if (keyboard["q"]) camera.position.y += actualSpeed;
-//   if (keyboard["e"]) camera.position.y += -actualSpeed;
-//   if (keyboard["w"]) camera.position.z += -actualSpeed;
-//   if (keyboard["s"]) camera.position.z += actualSpeed;
-//   if (keyboard["a"]) camera.position.x += -actualSpeed;
-//   if (keyboard["d"]) camera.position.x += +awactualSpeed;
-// };
-
-// console.log(scene.children[2].coupleColor == scene.children[3].coupleColor);
-// console.log(scene.children[3].material.color);
 
 const mainLoop = () => {
-  // let delta = clock.getDelta();
-  // processingKeyboard(delta);
-  // console.log(camera.position);
-  // orbitControls.update();
+  if (!gameOver) bufferSpeed += spawnSpeed;
+  if (bufferSpeed >= 1 && notVisibles.length) {
+    let spawnIndex = notVisibles.splice(
+      randomInRange(0, notVisibles.length - 1),
+      1
+    );
+    visibles.push(...spawnIndex);
+    scene.children[spawnIndex].visible = true;
+
+    bufferSpeed = 0;
+    spawnSpeed += 0.0001;
+  }
+
+  if (visibles.length == OBJECT_TOTAL) {
+    gameOver = true;
+    PLAY_BUTTON.disabled = false;
+    GAMEOVER.style.display = "block";
+  }
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(mainLoop);
--- Threejs/tugas-threejs-2/utilities.js
+++ Threejs/tugas-threejs-2/utilities.js
@@ -1,5 +1,15 @@
-const randomInRange = (from, to) => {
+const numberInRange = (from, to) => {
+  let arr = [];
+  for (let i = from; i <= to; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const randomInRange = (from, to, convertInt = false) => {
   let x = Math.random() * (to - from);
+
+  if (convertInt) return Math.floor(x + from);
   return x + from;
 };
 
@@ -10,11 +20,11 @@ const createCube = (color = Math.random() * 0xffffff) => {
   );
   cube.click = false;
   cube.name = "cube";
-  cube.visible = true;
+  cube.visible = false;
   cube.coupleColor = color;
-  cube.position.x = randomInRange(-30, 30);
-  cube.position.y = randomInRange(-30, 30);
-  cube.position.z = randomInRange(-30, 30);
+  cube.position.x = randomInRange(-20, 20);
+  cube.position.y = randomInRange(-20, 20);
+  cube.position.z = randomInRange(-20, 20);
   return cube;
 };
 
